3e75359a8a46e592a931c79dc1923875b511c61c
Joe Horvath <Joe.Horvath99@gmail.com>
2018-05-11 01:38:48 +1200

    Join the songs that we have genre for to all songs

--- loadMSD.m
+++ loadMSD.m
@@ -25,3 +25,18 @@ h5 = HDF5_Song_File_Reader(all_files{1});
 disp(['artist name is: ',h5.get_artist_name()]);
 disp([' song title is: ',h5.get_title()]);
 disp(['segment timbres: ',h5.get_segments_timbre()]);
+
+%load genre classifications
+data = importdata('MillionSongSubset/genreAssignment.txt');
+x = data(:,1); % column 1 of the data text file is assigned the variable x
+y = data(:,2); % column 2 is assigned the variable y
+genreSongs = [x,y];
+ids = strings(10000,1);
+for i=1:10000
+    h5 = HDF5_Song_File_Reader(all_files{i});
+    ids(i) = h5.get_track_id();
+end
+genreSongsTable = table(x,y,'VariableNames',{'id' 'genre'});
+idTable = table(cellfun(@(x) char(x),ids,'UniformOutput',false),'VariableNames',{'id'});
+genreSongsTable = unique(genreSongsTable);
+intersection = innerjoin(idTable,genreSongsTable);
