ca8f57964daf46a06adcc3fcc48a7a333bba84c1
Xavier Downs <xavier630@gmail.com>
2018-05-18 01:08:31 +1200

    Align names of features

--- CreateIntersection.m
+++ CreateIntersection.m
@@ -1,6 +1,6 @@
 function [intersection] = CreateIntersection(all_files)
 data = readtable('MillionSongSubset/genreAssignment.txt');
-data.Properties.VariableNames = {'id' 'Genre'};
+data.Properties.VariableNames = {'id' 'genre'};
 x = data(:,1); % column 1 of the data text file is assigned the variable x
 y = data(:,2); % column 2 is assigned the variable y
 ids = strings(10000,1);
