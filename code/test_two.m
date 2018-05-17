3177ffb0bde934d5f506b9a2712621d8340e7e3c
Lu Shien Lee <lushienlee@gmail.com>
2018-05-17 23:22:48 +1200

    Improve TDNN test

--- code/test_two.m
+++ code/test_two.m
@@ -17,14 +17,14 @@ g2 = [0;1;0;0];
 g3 = [0;0;1;0];
 g4 = [0;0;0;1];
 
-% song = zeros(3, 1000);
+% Getting a single feature for 4 different songs
 song(1,:) = test_get_feature(1, 1);
 song(2,:) = test_get_feature(2, 1);
 song(3,:) = test_get_feature(3, 1);
 song(4,:) = test_get_feature(4, 1);
 
 %  Create training set and targets
-len=250;
+len=125;
 len2 = len*2;
 smallInput = [song(1,1:len), song(2,1:len), song(3,1:len), song(4,1:len)];
 input = [smallInput, song(1, len+1:len2), song(2, len+1:len2), song(3,len+1:len2), song(4,len+1:len2)];
@@ -32,21 +32,32 @@ trainIn = tonndata(input, true, false);
 
 singleTarget = [repmat( g1,1,len ),repmat( g2,1,len ),repmat( g3,1,len ), repmat(g4,1,len)];
 target = [singleTarget, singleTarget];
+
+% Plotting input data
 figure();
+subplot(3,1,1);
 plot(transpose(target)); hold on;
 plot(transpose(input));
+title("Input Data");
 trainTarget = tonndata( target, true, false); 
 
+% Format and use data to train
 [Xs,Xi,Ai,Ts] = preparets(TDNN,trainIn, trainTarget); % Don't get affected by different delay size.
 TDNN = train(TDNN,Xs, Ts, Xi, Ai);
 
+% Feed the NN never-seen-before data
+[singleOut, fullOut] = getClassification(TDNN, transpose(song(1,len*3:len*3+len)));
 
-testAll = tonndata(smallInput, true, false);
-nnOutput = TDNN(testAll);
-mat = cell2mat(nnOutput);
-figure();
-plot(transpose(mat));
+% Plot results
+subplot(3,1,2);
+plot(fullOut);
 legend('1','2','3','4');
+title("Output wave for unseen data");
+subplot(3,1,3);
+bar(singleOut);
+ylim([0 1])
+title("Predicted genre of unseen data");
+
 
 % predicc{2} = mean(mat,2);
 
