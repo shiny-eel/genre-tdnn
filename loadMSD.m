d973658678842eaa7596af1f6f3f1f74d008e7e6
Joe Horvath <Joe.Horvath99@gmail.com>
2018-05-17 23:27:47 +1200

    Fixed loading the intersection

--- loadMSD.m
+++ loadMSD.m
@@ -27,17 +27,16 @@ fprintf('segment timbres len: %d\n', length(h5.get_segments_timbre()));
 
 %load genre classifications
 data = readtable('MillionSongSubset/genreAssignment.txt');
+data.Properties.VariableNames = {'id' 'Genre'};
 x = data(:,1); % column 1 of the data text file is assigned the variable x
 y = data(:,2); % column 2 is assigned the variable y
-genreSongs = [x,y];
 ids = strings(10000,1);
 for i=1:10000
     h5 = HDF5_Song_File_Reader(all_files{i});
     ids(i) = h5.get_track_id();
 end
-genreSongsTable = table(x,y,'VariableNames',{'id' 'genre'});
 idTable = table(cellfun(@(x) char(x),ids,'UniformOutput',false),'VariableNames',{'id'});
-genreSongsTable = unique(genreSongsTable);
+genreSongsTable = unique(data);
 intersection = innerjoin(idTable,genreSongsTable);
 
 % Load features
