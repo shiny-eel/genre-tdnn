39937d24a77952316c78ebc14e2cdb5193e35e4d
Lu Shien Lee <lushienlee@gmail.com>
2018-05-23 16:06:25 +1200

    Edit NN config

--- code/createTDNN.m
+++ code/createTDNN.m
@@ -1,10 +1,10 @@
 function [TDNN] = createTDNN()
 %TRAINNN Summary of this function goes here
 %   Detailed explanation goes here
-delay = 30;
-neurons = [30,30];
+delay = 50;
+neurons = [80,40];
 algo = 'trainscg';
-TDNN = timedelaynet((0:delay), neurons, char(algo));
+TDNN = timedelaynet((0:2:delay), neurons, char(algo));
             TDNN.trainParam.showWindow = false;
             TDNN.trainParam.showCommandLine = true;
 end
