4f981956fbbacc78ee3b9981837289626fec8ecc
Lu Shien Lee <lushienlee@gmail.com>
2018-05-23 16:09:39 +1200

    Remove old files

--- code/genreInput.m
+++
@@ -1,8 +0,0 @@
-function target = genreInput(genreChars,lengthSample)
-%GENREINPUT uses the genreToArray.m function to create
-% a t x n matrix 
-% where t is the number of timeslices specified by lengthSample
-% and n is the length of the genre format. (number of genres).
-target = repmat(genreToArray(genreChars), lengthSample, 1);
-end
-
