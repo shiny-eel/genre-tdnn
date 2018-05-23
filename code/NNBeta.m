3cf15fe58d34e112209e19f8efef4dfe3a519648
Lu Shien Lee <lushienlee@gmail.com>
2018-05-23 16:10:40 +1200

    Improve Beta test!!

--- code/NNBeta.m
+++ code/NNBeta.m
@@ -1,35 +1,51 @@
 % NNBeta.m is an example script of how to run the TDNN with the actual data
 clear;clc;close all;
 addpath('samples');
-load('samples/sample-with-genres.mat')
-numSongs = 24;
-numFeatures = 5;
-lenSample = 200;
- songs = transpose(table2array(train(1:numSongs, 'featureData')));
- 
-input = zeros(numSongs*lenSample,numFeatures);
-% Grab 400 timeslices from each song
-% input = [];
-index = 1;
-for song = songs
+dataset = load('samples/sample-with-table.mat');
+% load('tdnn-beta.mat', 'myTDNN');
+% 
+ myTDNN = createTDNN();
+% 
+ myTDNN = trainNN(myTDNN, dataset.trainIn, dataset.trainTarget);
+myTable = dataset.validTable;
+numSongs = height(myTable);
+Actual="";
+Predicted="";
+Correct=0;
+resultsTable = table(Actual,Predicted, Correct);
+% result = repmat(" ",[numSongs 2]);
+
+for i = 1:numSongs
+    featuresMatrix = getFeaturesFromTable(myTable(i, 'featureData'));
+    genreCell = myTable.genre(i);
+    genreChars = genreCell{:};
+    genreNameActual = string(genreChars);
     
-    disp("SS");
-    songData = cell2mat(song);
-    songSample = transpose(songData(1:5:25,1:lenSample));
-    indexEnd = index+lenSample;
-    input(index:indexEnd-1,:) = songSample;
-    index = indexEnd;
+    [singleOut, fullOut] = getClassification(myTDNN, featuresMatrix);
+    predictedGenre = arrayToGenre(singleOut);
+    
+%     result(i,:) = [genreNameActual, predictedGenre];
+    isCorrect = strcmpi(genreNameActual,predictedGenre);
+    resultsTable(i,:) = {genreNameActual, predictedGenre, isCorrect};
+%     resultsTable(i,'Predicted') = predictedGenre;
 end
 
-genres = (transpose(table2array(train(1:numSongs, 'Genre'))));
-for genre = genres
-    
-end
-
-
-myTDNN = createTDNN();
-myTDNN = trainNN(myTDNN, input, target);
-[singleOut, fullOut] = getClassification(myTDNN, input(1:600,:));
-plot(fullOut);
-legend('1','2','3','4');
-title("Output wave for training data");
+corrects = resultsTable.Correct;
+sumthat = sum(corrects, 1);
+fprintf("\n RESULT: TDNN correctly predicted " ...
+    +"%d out of %d song genres.\n", sumthat, numSongs);
+% [singleOut, fullOut] = getClassification(myTDNN, dataset.validIn(901:1200,:));
+% subplot(4,1,3);
+% plot(fullOut);
+% legend('1','2','3');
+% title("Output wave for training data");
+% xlabel("time");
+% ylabel("'Confidence' in genre");
+% 
+% 
+% subplot(4,1,4);
+% bar(singleOut);
+% ylim([0 1]);
+% title("Predicted genre of unseen data (Genre 3)");
+% xlabel("time");
+% ylabel("'Confidence' in genre");
