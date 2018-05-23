f6619289e5a53c5f40cd963fc4f9c256b0261e6f
Lu Shien Lee <lushienlee@gmail.com>
2018-05-23 16:22:13 +1200

    Adjust nn config

--- code/createTDNN.m
+++ code/createTDNN.m
@@ -1,8 +1,8 @@
 function [TDNN] = createTDNN()
 %TRAINNN Summary of this function goes here
 %   Detailed explanation goes here
-delay = 50;
-neurons = [80,40];
+delay = 40;
+neurons = [40,20];
 algo = 'trainscg';
 TDNN = timedelaynet((0:2:delay), neurons, char(algo));
             TDNN.trainParam.showWindow = false;
