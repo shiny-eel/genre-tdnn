f09a0399e1beda6c83fe6624506b9aaad7d18304
Lu Shien Lee <lushienlee@gmail.com>
2018-05-24 18:19:41 +1200

    Comments

--- loadMSD.m
+++ loadMSD.m
@@ -29,7 +29,8 @@ load('intersection.mat')
 
 % Load songs and features
 extractedSongs={};
-for currentSong = 1:numel(all_files)
+for currentSong = 1: numel(all_files)
+%     fprintf("\nProgress: %d out of %d", currentSong, numel(all_files));
     current_h5=HDF5_Song_File_Reader(all_files{currentSong});
     currentSongCell={cat(1, get_middle_elements(current_h5.get_segments_timbre()), get_middle_elements(current_h5.get_segments_pitches()), transpose(get_middle_elements(current_h5.get_segments_loudness_start())))};
     extractedSongs{currentSong, 1}=current_h5.get_track_id();
@@ -39,7 +40,8 @@ extractedSongTable=cell2table(extractedSongs, 'VariableNames', {'id' 'featureDat
 
 % Join table [id, features] with table [id, genre]
 finalIntersection = innerjoin(intersection, extractedSongTable);
-
+% data = finalIntersection;
+% save('samples/raw-all.mat', 'data');
 % TODOs
 % Retrieve middle n (eg 120) segments
 % Build two datasets rather than 1
