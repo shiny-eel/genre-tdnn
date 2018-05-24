b2e890f210af1e29df8f30fa39218ca4e6333ca0
Lu Shien Lee <lushienlee@gmail.com>
2018-05-24 23:17:02 +1200

    New NN script

--- code/NNGamma.m
+++ code/NNGamma.m
@@ -7,73 +7,35 @@ dataset = load('sample-allsongs-400s-5g.mat');
 load('code/lib/genres.mat', 'GenreSets');
 genres = GenreSets.five;
 % EITHER LOAD OR CREATE+TRAIN A TDNN
-MAKE_NEW = 0;
+MAKE_NEW = 1;
 if (MAKE_NEW)
     myTDNN = createTDNN();
-    myTDNN.trainParam.showWindow = true;
-
+%     myTDNN.trainParam.showWindow = ''''true;
+    
     myTDNN = trainNN(myTDNN, dataset.trainIn, dataset.trainTarget);
 else
-   load('cgbNN.mat', 'myTDNN');
-    myTDNN = trainNN(myTDNN, dataset.trainIn, dataset.trainTarget);
+    load('cgbNN.mat', 'myTDNN');
+    %     myTDNN = trainNN(myTDNN, dataset.trainIn, dataset.trainTarget);
 end
 
-% FORMAT A RESULTS TABLE
 myTable = dataset.validTable;
-Actual="";
-Predicted="";
-Correct=0;
-resultsTable = table(Actual,Predicted, Correct);
 
-numSongs = height(myTable);
-% LOOP THROUGH ALL VALIDATION SONGS
-genreArrayPredicted = zeros(numSongs, length(genres));
-for i = 1:numSongs
-    % Extract features
-    featuresMatrix = getFeaturesFromTable(myTable(i, 'featureData'));
-    % Extract true genre
-    genreCell = myTable.genre(i);
-    genreChars = genreCell{:};
-    genreNameActual = string(genreChars);
-    
-    % Plug input into nn
-    [singleOut, fullOut] = getClassification(myTDNN, featuresMatrix);
-    predictedGenre = arrayToGenre(singleOut);
-    genreArrayPredicted(i,:) = singleOut;
-    % Compare
-    isCorrect = strcmpi(genreNameActual,predictedGenre);    
-    resultsTable(i,:) = {genreNameActual, predictedGenre, isCorrect};
-    
-end
-    genreArrayActual = myTable.genreArray(:,:);
+resultsTable = validateNN(myTDNN, myTable);
+% subplot(2,1,1);
+p = plotResultsTable(resultsTable);
+title("Unseen validation set");
+% hold on;
 
-corrects = resultsTable.Correct;
-numCorrect = sum(corrects, 1);
-fprintf("\n RESULT: TDNN correctly predicted " ...
-    +"%d out of %d songs in %d genres.\n", numCorrect, numSongs, length(genres));
+load('samples/traintable-allsongs-400s-5g.mat', 'trainTable');
+otherresultsTable = validateNN(myTDNN, trainTable);
+% subplot(2,1,2);;;
+figure();
 
-% Confusion Matrix Plot
-p = plotconfusion(genreArrayActual.', genreArrayPredicted.');
-yticklabels(genres);
-xticklabels(genres);
-
+p2 = plotResultsTable(otherresultsTable);
+title("Training set");
 % SAVE the tdnn if it is good
 dir = 'tdnns/';
-filename = dir+"tdnn-"+numCorrect+"-correct.mat";
+INSERT="CHANGEME";
+filename = dir+"tdnn-"+INSERT+"-correct.mat";
 % save(filename, 'myTDNN');
 
-% [singleOut, fullOut] = getClassification(myTDNN, dataset.validIn(901:1200,:));
-% subplot(4,1,3);
-% plot(fullOut);
-% legend('1','2','3');
-% title("Output wave for training data");
-% xlabel("time");
-% ylabel("'Confidence' in genre");
-% 
-% 
-% subplot(4,1,4);
-% bar(singleOut);
-% ylim([0 1]);
-% title("Predicted genre of unseen data (Genre 3)");
-% xlabel("time");
-% ylabel("'Confidence' in genre");
